e80d9824766339a63e256d6466580861fcf03a6b
Manuel Krebber <admin@wheerd.de>
2026-02-28 00:21:12 +0100

    Increase screenshot tolerance

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,6 +33,12 @@ export default defineConfig({
     trace: 'on-first-retry'
   },
 
+  expect: {
+    toHaveScreenshot: {
+      maxDiffPixelRatio: 0.01
+    }
+  },
+
   /* Configure projects for major browsers */
   projects: [
     {
